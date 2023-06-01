a3a215c1f8de6e13c2a2780f223edd54db04e79e
MichaelFusionSanMateo <57538391+MichaelFusionSanMateo@users.noreply.github.com>
2023-06-01 10:04:29 -0700

    Update Exercise_01_01_MetabolicRates.java
    Made things look neater

--- Ch01/Exercise_01_01_MetabolicRates.java
+++ Ch01/Exercise_01_01_MetabolicRates.java
@@ -6,12 +6,24 @@ public class Exercise_01_01_MetabolicRates {
          * Sleeping 1 :Met
          * Sleep w/o eating to become ULTRA SLIM!
          * kilos = 150.0; it defaults as double?
+         * 
+         * Calories/Minute = .0175 * MET * Kg
+         * 
+         * Calories = .0175 * MET * Kg * Minute
+         * 
          */
+        final float CONVERSION_METS = 0.0175f;
+        final float CONVERSION_POUNDS_KILO = 2.2f;
         float calories;
-        float pounds = (float)150.0;
-        float kilograms = (pounds)/((float)2.2);
-        float mets = ((float)10.0 * 30) + ((float)8.0 * 30) + (float)360.0;
-        calories = ((float)0.0175) * mets * kilograms;
+        float pounds = 150;
+        float kilograms = pounds/CONVERSION_POUNDS_KILO;
+        float runningMets = 10;
+        float basketBallMets = 8;
+        float sleepingMets = 1;
+        float timeRunning = 30;
+        float timeBasketball = 30;
+        float timeSleep = 360;
+        calories = CONVERSION_METS * kilograms * (runningMets * timeRunning + basketBallMets*timeBasketball + sleepingMets * timeSleep);
         System.out.println(calories);
     }
 }
