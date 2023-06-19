45704acd7eba051bd2141b0312ea2e183bcc95a2
MichaelFusionSanMateo <57538391+MichaelFusionSanMateo@users.noreply.github.com>
2023-06-19 10:07:28 -0700

    Update PP_03ChildHight.java
    When we discussed why your program didn't work over zoom, we made these corrections together.

--- Ch03/Exercises/PP_03ChildHight.java
+++ Ch03/Exercises/PP_03ChildHight.java
@@ -1,101 +1,97 @@
-
+// Still need to make some changes
 import java.util.Scanner;
 public class PP_03ChildHight {
     public static void main(String[] args) {
-        String continueChecker = "y", feetAndInchesChecker = "Y", genderChecker;
+        String continueChecker = "y", feetAndInchesChecker, genderChecker;
         Scanner keyboard = new Scanner(System.in);
-        final int feetToInches = 12;
+        final int FEET_TO_INCHES = 12;
         int motherHightFeet, motherHightInches, fatherHightFeet, fatherHightInches, maleChildHightInches, maleChildHightFeet, femaleChildHightInches, femaleChildHightFeet;
         boolean genderFailLoop = true;
         while (continueChecker == "y" || continueChecker == "Y")
         {
-            System.out.println("Would you like to input the hights in feet and inches or just inches: Y for Feet N for Inches");
+            System.out.println("Would you like to input the height in feet and inches or just inches: Y for Feet N for Inches");
             feetAndInchesChecker = keyboard.nextLine();
-            keyboard.nextLine();
-            if (feetAndInchesChecker == "Y" || feetAndInchesChecker == "y")
-                {
-                    genderFailLoop = true;
-                    System.out.println("Please enter the mothers hight in feet then inches");
-                    motherHightFeet = keyboard.nextInt();
-                    motherHightInches = keyboard.nextInt();
-                    System.out.println("Please enter the fathers hight in feet then inches");
-                    fatherHightFeet = keyboard.nextInt();
-                    fatherHightInches = keyboard.nextInt();
+            if (feetAndInchesChecker.equals("Y") || feetAndInchesChecker.equals("y"))
+            {
+                genderFailLoop = true;
+                System.out.println("Please enter the mothers hight in feet then inches");
+                motherHightFeet = keyboard.nextInt();
+                motherHightInches = keyboard.nextInt();
+                System.out.println("Please enter the fathers hight in feet then inches");
+                fatherHightFeet = keyboard.nextInt();
+                fatherHightInches = keyboard.nextInt();
 
-                    fatherHightInches = (fatherHightFeet * feetToInches) + fatherHightInches;
-                    motherHightInches = (motherHightFeet * feetToInches) + motherHightInches;
-                    
-                    while (genderFailLoop == true)
-                    {
-                    System.out.println( "Please enter the gender of your child, M for male F for female");
-                    genderChecker = keyboard.nextLine();
-                    keyboard.nextLine();
-                    if (genderChecker == "M" || genderChecker == "m")
-                        {
-                            maleChildHightInches = ((motherHightInches * 13/12) + fatherHightInches);
-                            maleChildHightFeet = maleChildHightInches/12;
-                            maleChildHightInches =  maleChildHightInches % 12;
-                            System.out.println("Your child's hight is");
-                            System.out.println("Feet: " + maleChildHightFeet);
-                            System.out.println("Inches: " + maleChildHightInches);
-                            genderFailLoop = false;
-                            
-                        }
-                    else if (genderChecker == "F" || genderChecker == "f")
-                        {
-                            femaleChildHightInches = ((fatherHightInches * 12/13) + motherHightInches);
-                            femaleChildHightFeet = femaleChildHightInches/12;
-                            femaleChildHightInches =  femaleChildHightInches % 12;
-                            System.out.println("Your child's hight is");
-                            System.out.println("Feet: " + femaleChildHightFeet);
-                            System.out.println("Inches: " + femaleChildHightInches);
-                            genderFailLoop = false;
-                        }
-                    else
-                        {
-                            System.out.println("Lets try that again");
-                        }
+                fatherHightInches = (fatherHightFeet * FEET_TO_INCHES) + fatherHightInches;
+                motherHightInches = (motherHightFeet * FEET_TO_INCHES) + motherHightInches;
                 
-                    }
-                }
-            else if (feetAndInchesChecker == "N" || feetAndInchesChecker == "n")
+                while (genderFailLoop == true)
                 {
-                    genderFailLoop = true;
-                    System.out.println("Please enter the mothers in inches");
-                    motherHightInches = keyboard.nextInt();
-                    System.out.println("Please enter the fathers hight in inches");
-                    fatherHightInches = keyboard.nextInt();
-
-                    
-                    while (genderFailLoop == true)
+                System.out.println( "Please enter the gender of your child, M for male F for female");
+                genderChecker = keyboard.nextLine();
+                keyboard.nextLine();
+                if (genderChecker.equals("M") || genderChecker.equals("m"))
                     {
-                    System.out.println( "Please enter the gender of your child, M for male F for female");
-                    genderChecker = keyboard.nextLine();
-                    keyboard.nextLine();
-                    if (genderChecker == "M" || genderChecker == "m")
-                        {
-                            maleChildHightInches = ((motherHightInches * 13/12) + fatherHightInches);
-                            System.out.println("Your child's hight is");
-                            System.out.println("Inches: " + maleChildHightInches);
-                            genderFailLoop = false;
-                            
-                        }
-                    else if (genderChecker == "F" || genderChecker == "f")
-                        {
-                            femaleChildHightInches = ((fatherHightInches * 12/13) + motherHightInches);
-                            System.out.println("Your child's hight is");
-                            System.out.println("Inches: " + femaleChildHightInches);
-                            genderFailLoop = false;
-                        }
-                    else
-                        {
-                            System.out.println("Lets try that again");
-                        }
+                        maleChildHightInches = ((motherHightInches * 13/12) + fatherHightInches);
+                        maleChildHightFeet = maleChildHightInches/12;
+                        maleChildHightInches =  maleChildHightInches % 12;
+                        System.out.println("Your child's hight is");
+                        System.out.println("Feet: " + maleChildHightFeet);
+                        System.out.println("Inches: " + maleChildHightInches);
+                        genderFailLoop = false;
+                        
                     }
-
-
+                else if (genderChecker.equals("F") || genderChecker.equals("f"))
+                    {
+                        femaleChildHightInches = ((fatherHightInches * 12/13) + motherHightInches);
+                        femaleChildHightFeet = femaleChildHightInches/12;
+                        femaleChildHightInches =  femaleChildHightInches % 12;
+                        System.out.println("Your child's hight is");
+                        System.out.println("Feet: " + femaleChildHightFeet);
+                        System.out.println("Inches: " + femaleChildHightInches);
+                        genderFailLoop = false;
+                    }
+                else
+                    {
+                        System.out.println("Lets try that again");
+                    }            
                 }
-        }   }
-}
+            }
+            else if (feetAndInchesChecker == "N" || feetAndInchesChecker == "n")
+            {
+                genderFailLoop = true;
+                System.out.println("Please enter the mothers in inches");
+                motherHightInches = keyboard.nextInt();
+                System.out.println("Please enter the fathers hight in inches");
+                fatherHightInches = keyboard.nextInt();
 
-//currently broken idk why
+                
+                while (genderFailLoop == true)
+                {
+                System.out.println( "Please enter the gender of your child, M for male F for female");
+                genderChecker = keyboard.nextLine();
+                keyboard.nextLine();
+                if (genderChecker == "M" || genderChecker == "m")
+                    {
+                        maleChildHightInches = ((motherHightInches * 13/12) + fatherHightInches);
+                        System.out.println("Your child's hight is");
+                        System.out.println("Inches: " + maleChildHightInches);
+                        genderFailLoop = false;
+                        
+                    }
+                else if (genderChecker == "F" || genderChecker == "f")
+                    {
+                        femaleChildHightInches = ((fatherHightInches * 12/13) + motherHightInches);
+                        System.out.println("Your child's hight is");
+                        System.out.println("Inches: " + femaleChildHightInches);
+                        genderFailLoop = false;
+                    }
+                else
+                    {
+                        System.out.println("Lets try that again");
+                    }
+                }
+            }
+        }
+        keyboard.close();
+    }
+}
