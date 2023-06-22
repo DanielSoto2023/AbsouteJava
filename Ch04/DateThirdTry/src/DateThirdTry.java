55d2cdc4af79cdab3aff850e4793dead04dec074
DanielSoto2023 <danielsotofusion@gmail.com>
2023-06-22 10:26:17 -0700

    Added another display example from the book

--- Ch04/DateThirdTry/src/DateThirdTry.java
+++ Ch04/DateThirdTry/src/DateThirdTry.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner6;
-
-public class DateSecondTry {
+public class DateThirdTry {
 
 
     private String month;
@@ -13,6 +10,49 @@ public class DateSecondTry {
     {
         System.out.println(month + " " + day + ", " + year);
     }
+
+    public void setDate(int newMonth, int newDay, int newYear)
+    {
+        month = monthString(newMonth); // help
+        day = newDay;
+        year = newYear;
+    }
+
+    public String monthString(int monthNumber)
+    {
+        switch(monthNumber)
+        {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                System.out.println("Fatal Error");
+                System.exit(0);
+                return "Error"; //to keep the compiler happy
+        }
+    }
+
 
     
     public void readInput()
